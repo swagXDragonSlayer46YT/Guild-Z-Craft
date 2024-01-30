400102db63692b171d2f8ee701dc53b7e98abd0a
05council1 <128324236+05council1@users.noreply.github.com>
2024-01-30 01:51:04 -0500

    Added herobrine

--- overrides/groovy/postInit/main.groovy
+++ overrides/groovy/postInit/main.groovy
@@ -1,2 +1,9 @@
 
-println('Hello World!')
+println('Hello GuildZ.')
+import globals.Globals
+def LCR = recipeMap('large_chemical_reactor');
+
+LCR.recipeBuilder().EUt(480).duration(200)
+   .fluidInputs(fluid('water') * 1)
+   .fluidOutputs(fluid('lava') * 1)
+   .buildAndRegister()
